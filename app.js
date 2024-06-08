bc797f2502d163cc8e6376410cdc9bdc6dc0ae01
unknown <kendrick_sin@hotmail.com>
2024-06-08 13:04:17 +0800

    second commit

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200
-    res.setHeader('Content-Type', )
-})
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n I did this!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
